0027db69078b5c454ae362679721fcdaf0e70dcd
Pedro <pedro.gomes@grupomateus.com>
2025-06-18 11:44:18 -0300

    Alterations on code to add JavaScript Function on button: Baixar

--- back-end/app.py
+++ back-end/app.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 path_html = "centaurus/encartes_concorrentes.html"
 
-SCRIPTS = {
+SCRIPTS_FILES = {
     "assai.py",
     "atacadão.py",
     "frangolandia.py",
@@ -13,14 +13,21 @@ SCRIPTS = {
     "cometa.py"
 }
 
-
 def read_files():
-    set.add(SCRIPTS)
-    for i in SCRIPTS:
-        i(len(SCRIPTS))
+    set.add(SCRIPTS_FILES)
+    for i in SCRIPTS_FILES:
+        i(len(SCRIPTS_FILES))
     set.add(i)
     print(".")
     
+    if i in SCRIPTS_FILES([0]):
+        print("Not possible to make the script")
+        
+def loja_capture():
+    
+
+def output_js_file():
+        
     #Add js file to read and capture command in python scipts.
     #Read element on HTML with JS FIle
     #Add feature to capture if else paramenters of all stores
--- static/script.js
+++ static/script.js
@@ -1,21 +1,8 @@
-const elements = FileList(path)
-
-const path = "centaurus/models"
-
-function read_elements_in_html {
-    document.getElementById("clique").addEventListener("click") {
-        this.click.addEventListener("assai.py")
-    }
-}
-
-function event_script {
-    script = [0]
-    for i in path:
-        document.addEventListener("output.html")
-        if element not exists{
-            console.log("Not exists")
-        }
-    }
-
-
-function 
+document.addEventListener('DOMContentLoaded', function() {
+    const baixar =  document.getElementById("BaixarButão")
+    if (baixar) {
+        document.addEventListener("click", function() {
+            alert("Hello World");
+        });
+    };
+})
