ac813b6d4321e9a7c5b3d7ffa385e33a14874024
Pedro <pedro.gomes@grupomateus.com>
2025-06-26 10:12:15 -0300

    Changes

--- script.js
+++ script.js
@@ -1,68 +1,26 @@
-API_URL = "https://centursus-back-end-render.onrender.com"; // Seu URL base do Render
+const API_URL = "https://centursus-back-end-render.onrender.com";
 
-async function executeScript(loja) {
-    const response = await fetch(`${API_URL}/executar_script`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ loja: loja })
-    });
-    const data = await response.json();
-
-    if (!response.ok) {
-        console.error("Erro do backend:", data.message);
-        alert("Erro no servidor: " + data.message);s
-        return;
-    }
-
-    alert(data.message);
-
-    if (data.files && data.files.length > 0) {
-        const downloadContainer = document.getElementById('download-links-container');
-        if (!downloadContainer) {
-            console.error("Elemento 'download-links-container' não encontrado!");
-            return;
-        }
-        downloadContainer.innerHTML = '<h3>Arquivos Disponíveis:</h3>';
-
-        data.files.forEach(relativePath => {
-          
-            const fileUrl = `${API_URL}${relativePath}`;
-
-            const link = document.createElement('a');
-            link.href = fileUrl;
-            link.download = relativePath.split('/').pop(); 
-            link.textContent = `Baixar ${link.download}`;
-            link.style.display = 'block';
-            link.style.margin = '5px 0';
-            downloadContainer.appendChild(link);
-        });
-        alert("Arquivos estão prontos para download! Clique nos links.");
-    } else {
-        alert("Nenhum arquivo encontrado para download.");
-    }
-}
-
-async function ExecuteScript() {
-    const options = {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            "content-type": "application/json;charset=utf-8"
-        }
-    };
+async function fileUpload(file) {
+    const formData = new FormData();
+    formData.append('file', file); // use o parâmetro corretamente
 
     try {
-        const response = await fetch(`${API_URL}/AbrirDashRadio`, options);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+        const response = await fetch(`${API_URL}/fileUpload`, {
+            method: 'POST',
+            body: formData
+        });
+
         const data = await response.json();
-        console.log(data); 
-        return data;
+
+        if (!response.ok) {
+            console.error("Erro do backend:", data.message);
+            alert("Erro no servidor: " + data.message);
+            return;
+        }
+
+        alert(data.message);
     } catch (error) {
-        console.error("Error fetching data:", error);
-        
+        console.error("Erro na requisição:", error);
+        alert("Erro na conexão com o servidor.");
     }
 }
